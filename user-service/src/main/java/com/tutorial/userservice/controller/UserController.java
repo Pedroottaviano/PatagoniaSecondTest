a0ef1df00e7394676c094e54cb9fc429ea0e9146
pedro-ottaviano-globant <pedro.ottaviano@globant.com>
2022-06-23 11:38:46 -0300

    ready for pushing

--- user-service/src/main/java/com/tutorial/userservice/controller/UserController.java
+++ user-service/src/main/java/com/tutorial/userservice/controller/UserController.java
@@ -2,15 +2,12 @@ package com.tutorial.userservice.controller;
 
 import com.tutorial.userservice.entity.User;
 import com.tutorial.userservice.model.Bike;
-import com.tutorial.userservice.model.Car;
 import com.tutorial.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
@@ -41,15 +38,6 @@ public class UserController {
         return ResponseEntity.ok(userNew);
     }
 
-    @GetMapping("/cars/{userId}")
-    public ResponseEntity<List<Car>> getCars(@PathVariable("userId") int userId) {
-        User user = userService.getUserById(userId);
-        if(user == null)
-            return ResponseEntity.notFound().build();
-        List<Car> cars = userService.getCars(userId);
-        return ResponseEntity.ok(cars);
-    }
-
     @GetMapping("/bikes/{userId}")
     public ResponseEntity<List<Bike>> getBikes(@PathVariable("userId") int userId) {
         User user = userService.getUserById(userId);
@@ -59,14 +47,6 @@ public class UserController {
         return ResponseEntity.ok(bikes);
     }
 
-    @PostMapping("/savecar/{userId}")
-    public ResponseEntity<Car> saveCar(@PathVariable("userId") int userId, @RequestBody Car car) {
-        if(userService.getUserById(userId) == null)
-            return ResponseEntity.notFound().build();
-        Car carNew = userService.saveCar(userId, car);
-        return ResponseEntity.ok(car);
-    }
-
     @PostMapping("/savebike/{userId}")
     public ResponseEntity<Bike> saveBike(@PathVariable("userId") int userId, @RequestBody Bike bike) {
         if(userService.getUserById(userId) == null)
@@ -74,11 +54,4 @@ public class UserController {
         Bike bikeNew = userService.saveBike(userId, bike);
         return ResponseEntity.ok(bike);
     }
-
-    @GetMapping("/getAll/{userId}")
-    public ResponseEntity<Map<String, Object>> getAllVehicles(@PathVariable("userId") int userId) {
-        Map<String, Object> result = userService.getUserAndVehicles(userId);
-        return ResponseEntity.ok(result);
-    }
-
 }
--- user-service/src/main/java/com/tutorial/userservice/service/UserService.java
+++ user-service/src/main/java/com/tutorial/userservice/service/UserService.java
@@ -2,18 +2,12 @@ package com.tutorial.userservice.service;
 
 import com.tutorial.userservice.entity.User;
 import com.tutorial.userservice.feignclients.BikeFeignClient;
-import com.tutorial.userservice.feignclients.CarFeignClient;
 import com.tutorial.userservice.model.Bike;
-import com.tutorial.userservice.model.Car;
 import com.tutorial.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class UserService {
@@ -23,9 +17,6 @@ public class UserService {
 
     @Autowired
     RestTemplate restTemplate;
-
-    @Autowired
-    CarFeignClient carFeignClient;
 
     @Autowired
     BikeFeignClient bikeFeignClient;
@@ -43,46 +34,14 @@ public class UserService {
         return userNew;
     }
 
-    public List<Car> getCars(int userId) {
-        List<Car> cars = restTemplate.getForObject("http://localhost:8002/car/byuser/" + userId, List.class);
-        return cars;
-    }
-
     public List<Bike> getBikes(int userId) {
         List<Bike> bikes = restTemplate.getForObject("http://localhost:8003/bike/byuser/" + userId, List.class);
         return bikes;
-    }
-
-    public Car saveCar(int userId, Car car) {
-        car.setUserId(userId);
-        Car carNew = carFeignClient.save(car);
-        return carNew;
     }
 
     public Bike saveBike(int userId, Bike bike) {
         bike.setUserId(userId);
         Bike bikeNew = bikeFeignClient.save(bike);
         return bikeNew;
-    }
-
-    public Map<String, Object> getUserAndVehicles(int userId) {
-        Map<String, Object> result = new HashMap<>();
-        User user = userRepository.findById(userId).orElse(null);
-        if(user == null) {
-            result.put("Mensaje", "no existe el usuario");
-            return result;
-        }
-        result.put("User", user);
-        List<Car> cars = carFeignClient.getCars(userId);
-        if(cars.isEmpty())
-            result.put("Cars", "ese user no tiene coches");
-        else
-            result.put("Cars", cars);
-        List<Bike> bikes = bikeFeignClient.getBikes(userId);
-        if(bikes.isEmpty())
-            result.put("Bikes", "ese user no tiene motos");
-        else
-            result.put("Bikes", bikes);
-        return result;
     }
 }
